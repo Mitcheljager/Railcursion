d2bde0fd93680b84e5f30523dd965ce6bff96bbc
Mitchel <mitchel_jager@hotmail.com>
2023-05-16 21:49:36 +0200

    Adjust color and skybox

---
+++ Assets/Scripts/Performance/AnimationDistanceCulling.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class AnimationDistanceCulling : MonoBehaviour {
+    public float distanceThreshold = 10f; // Define your distance threshold here
+
+    private void Start() {
+        Animator animator = GetComponent<Animator>();
+        Transform cameraTransform = Camera.main.transform; // Assuming you're using the main camera, modify as needed
+
+        float distance = Vector3.Distance(transform.position, cameraTransform.position);
+
+        if (distance > distanceThreshold) animator.enabled = false;
+    }
+}
